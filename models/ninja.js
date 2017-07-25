447d43044c0195853d6c6610ceefea55496e748d
Hyunjung <trlee329@gmail.com>
2017-07-25 16:46:55 +0200

    test

---
+++ models/ninja.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create ninja Schema & Model
+const NinjaSchema = new Schema({
+  name: {
+    type: String,
+    required: [true,'Name field is required.']
+  },
+  rank: {
+    type: String
+  },
+  available: {
+    type: Boolean,
+    default: false
+  }
+
+  //add in geo location
+});
+
+const Ninja = mongoose.model('ninja', NinjaSchema);
+
+module.exports = Ninja;
